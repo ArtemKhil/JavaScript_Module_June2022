eb6c4d05f2ac11eb3db91c6eef35a8a36b2a28e4
ArtemKhil <artemkhilchenko09@gmail.com>
2022-07-25 10:33:50 +0100

    homework_lesson 1 completed.

--- homework_lesson1.js
+++ homework_lesson1.js
@@ -1,40 +1,92 @@
 // - Створити змінні. Присвоїти кожному з них значення:
 // 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 
-let greeting = 'hello';
-let str1 = 'owu';
-let str2 = 'com';
-let country = 'ua';
-let num1 = 1;
-let num2 = 10;
-let num3 = -999;
-let num4 = 123;
-const pi = 3.14;
-let num5 = 2.7;
-let num6 = 16;
-let bool1 = true;
-let bool2 = false;
+// let greeting = 'hello';
+// let str1 = 'owu';
+// let str2 = 'com';
+// let country = 'ua';
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// const pi = 3.14;
+// let num5 = 2.7;
+// let num6 = 16;
+// let bool1 = true;
+// let bool2 = false;
 
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
 
-console.log(greeting);
-console.log(str1);
-console.log(str2);
-console.log(country);
-console.log(num1);
-console.log(num2);
-console.log(num3);
-console.log(num4);
-console.log(pi);
-console.log(num5);
-console.log(num6);
-console.log(bool1);
-console.log(bool2);
-
-
-// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// console.log(greeting);
+// console.log(str1);
+// console.log(str2);
+// console.log(country);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(pi);
+// console.log(num5);
+// console.log(num6);
+// console.log(bool1);
+// console.log(bool2);
 //
+// alert(greeting);
+// alert(str1);
+// alert(str2);
+// alert(country);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(pi);
+// alert(num5);
+// alert(num6);
+// alert(bool1);
+// alert(bool2);
+//
+// document.write(greeting);
+// document.write(str1);
+// document.write(str2);
+// document.write(country);
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(num4);
+// document.write(pi);
+// document.write(num5);
+// document.write(num6);
+// document.write(bool1);
+// document.write(bool2);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// const firstName = 'Artem';
+// const middleName = 'Viktor';
+// const lastName = 'Khilchenko';
+//
+// const person = `${firstName} ${middleName} ${lastName}`;
+//
+// console.log(person);
+
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
 //
-// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// - За допомогою 3х різних prompt() отримати 3 слова,
+// які являються вашими Імям, По-Батькові та роками. та вивести в консоль.
+
+// let firstName = prompt('What is your name?');
+// let lastName = prompt('What is surname?');
+// let age = prompt('How old are you?');
+// console.log(firstName, lastName, age);
+
+
+
