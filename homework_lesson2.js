6eaf9af2f2f22a08c3233e03e1b533c09f9c3f56
ArtemKhil <artemkhilchenko09@gmail.com>
2022-07-27 11:45:30 +0100

    homework_lesson 2 completed.

---
+++ homework_lesson2.js
@@ -0,0 +1,127 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль.
+
+// let array = ['hello', 23, true, 'user', 34, false, 'age', 165, 'status', true];
+//
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+//
+// let book1 = {
+//     title: 'upgrade',
+//     pageCount: 341,
+//     genre: 'novel'
+// };
+//
+// let book2 = {
+//     title: 'rising tiger',
+//     pageCount: 325,
+//     genre: 'thriller'
+// };
+//
+// let book3 = {
+//     title: 'the house across the lake',
+//     pageCount: 349,
+//     genre: 'novel'
+// };
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, author.
+// Поле "автори" - масив. Кожен автор має поля name,age
+//
+// let book4 = {
+//     title: 'escape',
+//     pageCount: 356,
+//     genre: 'thriller',
+//     author: [name = 'james patterson', age = 67]
+// };
+//
+// let book5 = {
+//     title: 'the it girl',
+//     pageCount: 422,
+//     genre: 'drama',
+//     author: [name = 'ruth ware', age = 56]
+// };
+//
+// let book6 = {
+//     title: 'the hotel nantucket',
+//     pageCount: 345,
+//     genre: 'novel',
+//     author: [name = 'elin hilderbrand', age = 45]
+// };
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let users = [
+//     user1 = {
+//         name: 'John',
+//         username: 'Johnny',
+//         password: 'watermelon777'
+//     },
+//     user2 = {
+//         name: 'Jack',
+//         username: 'Jackson',
+//         password: 'apple34'
+//     },
+//     user3 = {
+//         name: 'Andrew',
+//         username: 'AndrewMac',
+//         password: 'table786'
+//     },
+//     user4 = {
+//         name: 'Steve',
+//         username: 'StevieG',
+//         password: 'passion0786'
+//     },
+//     user5 = {
+//         name: 'Roger',
+//         username: 'RogerThat',
+//         password: 'counter3460'
+//     },
+//     user6 = {
+//         name: 'Samuel',
+//         username: 'SamLarkin',
+//         password: 'waitrose5490'
+//     },
+//     user7 = {
+//         name: 'Peter',
+//         username: 'PeterPen',
+//         password: 'strawberry2345'
+//     },
+//     user8 = {
+//         name: 'Sebastian',
+//         username: 'Seb',
+//         password: 'cucumber8930'
+//     },
+//     user9 = {
+//         name: 'Emma',
+//         username: 'EmmaWatson',
+//         password: 'bentley509'
+//     },
+//     user10 = {
+//         name: 'Victoria',
+//         username: 'Queen',
+//         password: 'king45789'
+//     }
+//
+// ];
+//
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
+
